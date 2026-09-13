e9cfb18d52bd53727f6f83d5255ac298aa49c067
Aryue <aryue1945@gmail.com>
2026-09-13 09:17:44 -0700

    Connect player to attack framework

--- Player.cs
+++ Player.cs
@@ -1,6 +1,6 @@
 using Godot;
 
-public partial class Player : CharacterBody2D
+public partial class Player : CharacterBody2D, ICombatant
 {
 	private const float TileSize = 32.0f;
 	private Polygon2D _facingIndicator;
@@ -15,7 +15,11 @@ public partial class Player : CharacterBody2D
 	public delegate void DiedEventHandler();
 
 	public int Health { get; private set; } = 3;
-	public int AttackRange { get; private set; } = 1;
+	public bool IsAlive => Health > 0;
+	public CombatFaction Faction => CombatFaction.Player;
+	public AttackState Attack { get; } = new(
+		AttackDefinitions.BasicStrike
+	);
 
 	public override void _Ready()
 	{
@@ -61,10 +65,6 @@ public partial class Player : CharacterBody2D
 		Position += direction * TileSize;
 	}
 
-	public void SetAttackRange(int attackRange)
-	{
-		AttackRange = attackRange < 1 ? 1 : attackRange;
-	}
 
 	private void SetFacingDirection(Vector2 direction)
 	{
